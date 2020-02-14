084ac55b5c4f9b0dac76523d73cba1b3ee906f7f
Andrey <56509187+Andrey1qwe1@users.noreply.github.com>
2020-02-14 21:12:51 +0700

    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -16,16 +16,46 @@ for (var i = 0; i < inp.length; ++i) {
 var mobMenu = document.querySelector('.mob-menu');
 var navigation = document.querySelector('.navigation');
 var navigationList = document.querySelector('.navigation__list');
+var subMenu = document.querySelector('.navigation__submenu');
+var navigationLink = document.querySelectorAll('.navigation__link');
 
 mobMenu.onclick = function() {
 	if (navigationList.classList.contains('navigation__list_shown')) {
 		navigationList.classList.remove('navigation__list_shown');
 		navigationList.classList.add('navigation__list_hidden');
-
+		for (var i=0; i < navigationLink.length; ++i){
+			if(navigationLink[i].nextElementSibling != null) {
+			navigationLink[i].nextElementSibling.classList.remove('navigation__submenu_shown');
+			}
+			if (navigationLink[i].classList.contains('navigation__item_mob-active')) {
+				navigationLink[i].classList.remove('navigation__item_mob-active');
+			}
+		}
 	}
 	else {
-		navigationList.classList.remove('navigation__list_hidden')
+		navigationList.classList.remove('navigation__list_hidden');
 		navigationList.classList.add('navigation__list_shown');
 	}
 };
+
+for (var i=0; i < navigationLink.length; ++i){
+	navigationLink[i].onclick = function() {
+		if (this.classList.contains('navigation__item_mob-active')) {
+			this.classList.remove('navigation__item_mob-active');
+			if(this.nextElementSibling != null) {
+			this.nextElementSibling.classList.add('navigation__submenu_hidden');
+			this.nextElementSibling.classList.remove('navigation__submenu_shown');
+			}
+
+		}
+		else {
+			this.classList.add('navigation__item_mob-active');
+			if(this.nextElementSibling != null) {
+			this.nextElementSibling.classList.add('navigation__submenu_shown');
+			this.nextElementSibling.classList.remove('navigation__submenu_hidden');
+			}
+		}
+	};
+}
+
 
