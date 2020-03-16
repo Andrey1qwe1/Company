8ac2a8e9600ed0d3981bcd85db63be08e3a7ccb7
Andrey <56509187+Andrey1qwe1@users.noreply.github.com>
2020-03-16 21:04:27 +0700

    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -2,30 +2,27 @@ var inp = document.querySelectorAll('input');
 for (var i = 0; i < inp.length; ++i) {
 	inp[i].onblur = function () {
 		if (this.value.length > 0) {
-			this.style.background = 'linear-gradient(0deg, rgba(253,255,255,1) 0%, rgba(176,183,184,1) 100%)';
+			this.classList.add('input_active');
 		}
 		else {
-			this.style.background = '#687072';
+			if(this.classList.contains('input_active')) {
+				this.classList.remove('input_active');
+			}
 		}
 	};
 	inp[i].onfocus = function () {
-		this.style.background = 'linear-gradient(0deg, rgba(253,255,255,1) 0%, rgba(176,183,184,1) 100%)';	
+		this.classList.add('input_active');
 	};
 }
-
-var mobMenu = document.querySelector('.mob-menu');
-var navigation = document.querySelector('.navigation');
 var navigationList = document.querySelector('.navigation__list');
-var subMenu = document.querySelector('.navigation__submenu');
 var navigationLink = document.querySelectorAll('.navigation__link');
-
-mobMenu.onclick = function() {
+document.querySelector('.mob-menu').onclick = function() {
 	if (navigationList.classList.contains('navigation__list_shown')) {
 		navigationList.classList.remove('navigation__list_shown');
 		navigationList.classList.add('navigation__list_hidden');
 		for (var i=0; i < navigationLink.length; ++i){
 			if(navigationLink[i].nextElementSibling != null) {
-			navigationLink[i].nextElementSibling.classList.remove('navigation__submenu_shown');
+				navigationLink[i].nextElementSibling.classList.remove('navigation__submenu_shown');
 			}
 			if (navigationLink[i].classList.contains('navigation__item_mob-active')) {
 				navigationLink[i].classList.remove('navigation__item_mob-active');
@@ -37,22 +34,20 @@ mobMenu.onclick = function() {
 		navigationList.classList.add('navigation__list_shown');
 	}
 };
-
 for (var i=0; i < navigationLink.length; ++i){
 	navigationLink[i].onclick = function() {
 		if (this.classList.contains('navigation__item_mob-active')) {
 			this.classList.remove('navigation__item_mob-active');
 			if(this.nextElementSibling != null) {
-			this.nextElementSibling.classList.add('navigation__submenu_hidden');
-			this.nextElementSibling.classList.remove('navigation__submenu_shown');
+				this.nextElementSibling.classList.add('navigation__submenu_hidden');
+				this.nextElementSibling.classList.remove('navigation__submenu_shown');
 			}
-
 		}
 		else {
 			this.classList.add('navigation__item_mob-active');
 			if(this.nextElementSibling != null) {
-			this.nextElementSibling.classList.add('navigation__submenu_shown');
-			this.nextElementSibling.classList.remove('navigation__submenu_hidden');
+				this.nextElementSibling.classList.add('navigation__submenu_shown');
+				this.nextElementSibling.classList.remove('navigation__submenu_hidden');
 			}
 		}
 	};
